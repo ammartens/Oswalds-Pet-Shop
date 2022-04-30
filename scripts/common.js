9f9fa5587b7bb2ad99a3a08e2b87947e7f7f53d2
Ariana <96448173+ammartens@users.noreply.github.com>
2022-04-29 18:50:46 -0700

    some asthetic work

--- scripts/common.js
+++ scripts/common.js
@@ -27,10 +27,29 @@
         }
     this.showNextPromoItem = function () {
     /* fade out items */
+        $(self.promoBar.promoItems).fadeOut("slow").promise().
+            done(function () {
+                /*current promo item counter */
+                if (self.promoBar.currentItem >= (self.promoBar.numberOfItems - 1)) {
+                    /* reset counter to zero */
+                    self.promoBar.currentItem = 0;
+                } else {
+                    /* increease by 1 */
+                    self.promoBar.currentItem++;
+                }
+                /* fade to next item*/
+                $(self.promoBar.promoItems).eq(self.promoBar.currentItem).
+                    fadeIn("slow", function () {
+                        /* delay before showing next item */
+                        self.startDelay();
+                    });
+            });
 
     }
 }
 $(document).ready(function () {
     /* instantiate new common class */
     app.common = new Common();
+/*initialize promo bar */
+    app.common.initialisePromo();
 });
